ceaca576340aeda14ed6980e4e5fe615257d0919
Ramesh Ramasamy <rameshramasamymca@gmail.com>
2022-05-29 21:11:20 +0530

    Fixing bugs

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -1,35 +1,48 @@
 class PeopleController
   def initialize(params)
     @params = params
+    puts @params
   end
 
   def normalize
     people = []
-    people << get_normalized_people_by_dollar(@params.dollar_format)
-    people << get_normalized_people_by_percent(@params.percent_format)
-    sort_by(people, @params.order)
+    people += get_normalized_people_by_dollar(@params[:dollar_format])
+    people += get_normalized_people_by_percent(@params[:percent_format])
+    people = sort_by(people, @params[:order])
+    people.map{|p| [p.first_name, p.city, p.birth_date].join(", ") }
   end
 
   private
   def get_normalized_people_by_dollar(raw_people_str)
-    people = []
-    raw_people_str.split("\n").map do |people_with_dollar|
-      city, birth_date, last_name, first_name = people_with_dollar.split("$").map(&:stripe)
-      people << People.new(first_name, city, birth_date, last_name)
+    raw_people_str.split("\n")[1..-1].map do |people_with_dollar|
+      city, birth_date, last_name, first_name = people_with_dollar.split("$").map(&:strip!)
+      p = ::People.new(first_name, city, birth_date, last_name)
+      ::People.new(first_name, city, birth_date, last_name)
     end
-    return people
+  end
 
   def get_normalized_people_by_percent(raw_people_str)
-    people = []
-    aw_people_str.split("\n").map do |people_by_percent|
-      first_name, city, birth_date = people_by_percent.split("$").map(&:stripe)
-      people << People.new(first_name, city, birth_date, nil)
+    raw_people_str.split("\n")[1..-1].map do |people_by_percent|
+      first_name, city, birth_date = people_by_percent.split("%").map(&:strip!)
+      ::People.new(first_name, city, birth_date, nil)
     end
-    return people
+  end
 
   def sort_by(people, field)
-    people.sort_by(field)
+    people.sort_by{|p| p.send(field)}
   end
 
   attr_reader :params
+end
+
+#Since model people is not loading.. copy pasted here.
+
+class People
+  attr_reader  :first_name, :city, :birth_date, :last_name
+  def initialize(first_name, city, birth_date, last_name)
+      @first_name = first_name
+      @city = city
+      @birth_date = birth_date
+      @last_name = last_name
+   end
 end
